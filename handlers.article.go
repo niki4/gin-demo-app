5ea2265fb2f1a2f6607539d70a2a254b41b62d3e
niki4 <nikiforr@gmail.com>
2020-12-28 18:16:35 +0300

    Add single article route/handler

--- handlers.article.go
+++ handlers.article.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func showIndexPage(c *gin.Context) {
@@ -15,4 +16,24 @@ func showIndexPage(c *gin.Context) {
 			"title":   "Home Page",
 			"payload": articles,
 		})
+}
+
+func getArticle(c *gin.Context) {
+	// Check if the Article ID is valid
+	if articleID, err := strconv.Atoi(c.Param("article_id")); err == nil {
+		// Check if the Article exists
+		if article, err := getArticleByID(articleID); err == nil {
+			c.HTML(
+				http.StatusOK,
+				"article.html",
+				gin.H{
+					"title":   article.Title,
+					"payload": article,
+				})
+		} else {
+			c.AbortWithError(http.StatusNotFound, err)
+		}
+	} else {
+		c.AbortWithError(http.StatusNotFound, err)
+	}
 }
