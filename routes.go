a023292db54f3fbed5ea9d0b083c2b9090dcaa97
niki4 <nikiforr@gmail.com>
2020-12-28 17:57:36 +0300

    Migrate showIndexPage to dedicated module

--- routes.go
+++ routes.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"net/http"
-)
-
-func showIndexPage(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", gin.H{"title": "Home Page"})
-}
-
 func initializeRoutes() {
 	router.GET("/", showIndexPage)
 }
